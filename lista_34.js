a2ceb4f15465353365ed77fd9a835d7353bfa524
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-10-11 20:52:27 -0300

    Exercicio 1 finalizado

--- lista_34.js
+++ lista_34.js
@@ -1,1 +1,18 @@
 // OLÁ MUNDO
+var palavraUm = "amadurecida";
+var palavraDois = "agradecida";
+console.log(VerificarPalavra(palavraUm, palavraDois));
+function VerificarPalavra(paramentroUm, parametroDois) {
+    var resultado = false;
+    var arrayUm = paramentroUm.split("");
+    var arrayDois = parametroDois.split("");
+    var tamanhoUm = arrayUm.length;
+    var tamanhoDois = arrayDois.length;
+    console.log(arrayUm);
+    console.log(arrayDois);
+    if (arrayUm[tamanhoUm - 3] == arrayDois[tamanhoDois - 3] && arrayUm[tamanhoUm - 2] == arrayDois[tamanhoDois - 2] && arrayUm[tamanhoUm - 1] == arrayDois[tamanhoDois - 1]) {
+        resultado = true;
+        return resultado;
+    }
+    return resultado;
+}
--- lista_34.ts
+++ lista_34.ts
@@ -1,1 +1,25 @@
 // OLÁ MUNDO
+
+let palavraUm : string = "amadurecida"
+let palavraDois : string = "agradecida"
+
+console.log(VerificarPalavra(palavraUm, palavraDois))
+
+function VerificarPalavra(paramentroUm:string, parametroDois:string): boolean{
+    let resultado : boolean = false
+    let arrayUm:string[] = paramentroUm.split("")
+    let arrayDois:string[] = parametroDois.split("")
+    let tamanhoUm : number = arrayUm.length
+    let tamanhoDois : number = arrayDois.length
+
+    console.log(arrayUm)
+    console.log(arrayDois)
+
+    if(arrayUm[tamanhoUm - 3] == arrayDois[tamanhoDois - 3] && arrayUm[tamanhoUm - 2] == arrayDois[tamanhoDois - 2] && arrayUm[tamanhoUm - 1] == arrayDois[tamanhoDois - 1]){
+        resultado = true
+        return resultado
+    }
+
+    return resultado
+   
+}
