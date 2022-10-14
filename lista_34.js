72af7b44696394610d8085933b846929a01c4c62
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-10-13 21:05:24 -0300

    Exercicio 2 finalizado

--- lista_34.js
+++ lista_34.js
@@ -1,18 +1,36 @@
 // OLÁ MUNDO
-var palavraUm = "amadurecida";
-var palavraDois = "agradecida";
-console.log(VerificarPalavra(palavraUm, palavraDois));
-function VerificarPalavra(paramentroUm, parametroDois) {
-    var resultado = false;
-    var arrayUm = paramentroUm.split("");
-    var arrayDois = parametroDois.split("");
-    var tamanhoUm = arrayUm.length;
-    var tamanhoDois = arrayDois.length;
-    console.log(arrayUm);
-    console.log(arrayDois);
-    if (arrayUm[tamanhoUm - 3] == arrayDois[tamanhoDois - 3] && arrayUm[tamanhoUm - 2] == arrayDois[tamanhoDois - 2] && arrayUm[tamanhoUm - 1] == arrayDois[tamanhoDois - 1]) {
-        resultado = true;
-        return resultado;
-    }
-    return resultado;
+//EXERCICIO 1
+// let palavraUm : string = "amadurecida"
+// let palavraDois : string = "agradecida"
+// console.log(VerificarPalavra(palavraUm, palavraDois))
+// function VerificarPalavra(paramentroUm:string, parametroDois:string): boolean{
+//     let resultado : boolean = false
+//     let arrayUm:string[] = paramentroUm.split("")
+//     let arrayDois:string[] = parametroDois.split("")
+//     let tamanhoUm : number = arrayUm.length
+//     let tamanhoDois : number = arrayDois.length
+//     console.log(arrayUm)
+//     console.log(arrayDois)
+//     if(arrayUm[tamanhoUm - 3] == arrayDois[tamanhoDois - 3] && arrayUm[tamanhoUm - 2] == arrayDois[tamanhoDois - 2] && arrayUm[tamanhoUm - 1] == arrayDois[tamanhoDois - 1]){
+//         resultado = true
+//         return resultado
+//     }
+//     return resultado
+// }
+//EXERCICIO 2
+var palavra = "teste";
+console.log(ContarCaractere(palavra));
+function ContarCaractere(parametro) {
+    var parametroArray = parametro.split("");
+    var arrayFinal = [];
+    parametroArray.forEach(function (x) {
+        var contador = parametroArray.filter(function (y) { return y == x; });
+        if (contador.length == 1) {
+            arrayFinal.push("(");
+        }
+        else {
+            arrayFinal.push(")");
+        }
+    });
+    return arrayFinal.join("");
 }
--- lista_34.ts
+++ lista_34.ts
@@ -1,25 +1,48 @@
 // OLÁ MUNDO
 
-let palavraUm : string = "amadurecida"
-let palavraDois : string = "agradecida"
+//EXERCICIO 1
+// let palavraUm : string = "amadurecida"
+// let palavraDois : string = "agradecida"
 
-console.log(VerificarPalavra(palavraUm, palavraDois))
+// console.log(VerificarPalavra(palavraUm, palavraDois))
 
-function VerificarPalavra(paramentroUm:string, parametroDois:string): boolean{
-    let resultado : boolean = false
-    let arrayUm:string[] = paramentroUm.split("")
-    let arrayDois:string[] = parametroDois.split("")
-    let tamanhoUm : number = arrayUm.length
-    let tamanhoDois : number = arrayDois.length
+// function VerificarPalavra(paramentroUm:string, parametroDois:string): boolean{
+//     let resultado : boolean = false
+//     let arrayUm:string[] = paramentroUm.split("")
+//     let arrayDois:string[] = parametroDois.split("")
+//     let tamanhoUm : number = arrayUm.length
+//     let tamanhoDois : number = arrayDois.length
 
-    console.log(arrayUm)
-    console.log(arrayDois)
+//     console.log(arrayUm)
+//     console.log(arrayDois)
 
-    if(arrayUm[tamanhoUm - 3] == arrayDois[tamanhoDois - 3] && arrayUm[tamanhoUm - 2] == arrayDois[tamanhoDois - 2] && arrayUm[tamanhoUm - 1] == arrayDois[tamanhoDois - 1]){
-        resultado = true
-        return resultado
-    }
+//     if(arrayUm[tamanhoUm - 3] == arrayDois[tamanhoDois - 3] && arrayUm[tamanhoUm - 2] == arrayDois[tamanhoDois - 2] && arrayUm[tamanhoUm - 1] == arrayDois[tamanhoDois - 1]){
+//         resultado = true
+//         return resultado
+//     }
 
-    return resultado
+//     return resultado
    
+// }
+
+//EXERCICIO 2
+
+let palavra : string = "teste"
+
+console.log(ContarCaractere(palavra))
+
+function ContarCaractere(parametro:string): string{
+   let parametroArray: string[] = parametro.split("")
+   let arrayFinal: string[] = []
+
+   parametroArray.forEach(x => {
+    let contador: string[] = parametroArray.filter(y => y == x)
+    if(contador.length == 1){
+        arrayFinal.push("(")
+    }
+    else{
+        arrayFinal.push(")")
+    }
+   })
+   return arrayFinal.join("")
 }
